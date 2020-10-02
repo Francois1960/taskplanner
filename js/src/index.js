8d2f382b73dfad56bed473bf50f7f529a9c2ea21
Francois <francois.wentzel@bigpond.com>
2020-10-03 08:28:19 +1000

    change file path index.HTML and add HTML validation

--- js/src/index.js
+++ js/src/index.js
@@ -1,9 +1,9 @@
 
 const taskManager = new TaskManager(0);
 //taskManager.addTask('Drink Coffee', 'make a lot of expresso', 'Joe', '25/09/2020');
-
+taskManager.load();
 //console.log(taskManager.tasks)
-
+taskManager.render();
 const newtaskForm = document.querySelector('#newTaskForm');
 
 newTaskForm.addEventListener('submit',(event)=>{
@@ -13,7 +13,30 @@ newTaskForm.addEventListener('submit',(event)=>{
     const newAssignedTo=document.querySelector('#newAssignedTo');
     const newDueDate=document.querySelector('#newDueDate');
    
-//validation code here
+//validation code hereCreate a JavaScript function called “validateTaskForm” that verifies that the inputs inserted by the user in the task form are correct:
+//Name -> Not Empty and longer than 8 characters
+//Description -> Not Empty and longer than 15 characters
+//AssignedTo -> Not Empty and longer than 8 characters
+// DueDate  -> Not Empty and not in the past
+
+ /* if(newNameOfTask.value.length <= 0 && newNameOfTask.value.length >= 8){
+      alert("success");
+  }
+  else{
+      alert("make sure the input is between more than 8 characters long")
+  }
+}
+const validateTaskForm()=>{
+  function validate(){
+    x=document.myForm
+    input=x.myInput.value
+    if (input.length>5){
+        alert("The field cannot contain more than 5 characters!")
+        return false
+    }else {
+        return true
+
+playing here*/
 
     const nameOfTask = newNameOfTask.value;
     const description = newDescription.value;
@@ -48,5 +71,5 @@ newTaskForm.addEventListener('submit',(event)=>{
         taskManager.save();
         taskManager.render();
       
-}
+      }
 });
